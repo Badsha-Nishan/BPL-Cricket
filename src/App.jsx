ee1de2ae65d5e931e10a61d657b8ef08f56726cd
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-24 00:02:48 +0600

    initalized the BPL Project with added navBar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import navImg from "./assets/logo.png";
+import Coin from "./assets/dollar-1.png";
+
+function App() {
+  return (
+    <>
+      <div className="navbar w-11/12 mx-auto mt-6">
+        <div className="flex-1">
+          <a className="text-xl">
+            <img className="h-16 w-16" src={navImg} alt="Nave Logo" />
+          </a>
+        </div>
+        <div className="flex items-center">
+          <span className="mr-3">60000000000</span>
+          <span className="mr-3">Coin</span>
+          <img src={Coin} alt="Coin" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
